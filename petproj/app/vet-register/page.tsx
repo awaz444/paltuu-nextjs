1260d90372ffba4113733788078ed4a10e17f3c5
Aawaiz <aawaizali8@gmail.com>
2025-03-18 22:18:44 +0500

    clinic email and ehatsapp not necessary

--- petproj/app/vet-register/page.tsx
+++ petproj/app/vet-register/page.tsx
@@ -180,7 +180,7 @@ const VetRegisterForm = () => {
                             onChange={(e) => setClinicEmail(e.target.value)}
                             placeholder="contact@myvetclinic.com"
                             className="w-full border border-gray-300 rounded-xl px-3 py-2 focus:ring-2 focus:ring-primary focus:outline-none"
-                            required
+                        
                         />
                     </div>
 
@@ -204,7 +204,7 @@ const VetRegisterForm = () => {
                                 onChange={(e) => setClinicWhatsapp(e.target.value)}
                                 placeholder="3338888666"
                                 className="w-full border border-gray-300 rounded-xl px-3 py-2 focus:ring-2 focus:ring-primary focus:outline-none"
-                                required
+                          
                             />
                         </div>
                     </div>

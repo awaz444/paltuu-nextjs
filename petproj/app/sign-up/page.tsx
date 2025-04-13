1b66468f83bbf61d1d5010b0857e4a00250d5ab4
Aawaiz Ali <aawaizali8@.gmail.com>
2025-04-13 13:05:45 +0500

    confetti + minor changes

--- petproj/app/sign-up/page.tsx
+++ petproj/app/sign-up/page.tsx
@@ -300,7 +300,7 @@ const CreateUser = () => {
                 <img
                     src="/paltu_logo.svg"
                     alt="Paltu Logo"
-                    className="mb-6 w-40 lg:w-full max-w-full"
+                    className="mb-3 mt-2 w-40 lg:w-full max-w-full"
                 />
             </div>
 
@@ -313,7 +313,7 @@ const CreateUser = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clipRule="evenodd" />
                     </svg>
-                    Back to Founders Club
+                    
                 </button> }
 
                 <form

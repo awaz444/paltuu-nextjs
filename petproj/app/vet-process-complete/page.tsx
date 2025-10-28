0d1161662d06b3527369e8619efbaee1dec81d60
Aawaiz <aawaizali8@gmail.com>
2025-10-28 16:38:10 +0500

    closing bracket on reviews

--- petproj/app/vet-process-complete/page.tsx
+++ petproj/app/vet-process-complete/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 import React, { useEffect, useState } from "react";
-import Navbar from "@/components/navbar";
 
 const ListingCreated = () => {
     return (
@@ -28,4 +26,3 @@ const ListingCreated = () => {
 };
 
 export default ListingCreated;
-
